f85cd947ebd16763186bc94153665f2cf1d61d5a
valerypiniazik <>
2020-02-16 22:05:54 +0300

    added lines clear functionality

--- client/src/components/game_engine/index.js
+++ client/src/components/game_engine/index.js
@@ -4,7 +4,7 @@ import GameLoop from '../../game_engine/game_loop';
 import GameVerifier from '../../game_engine/game_verifier';
 import KeysHandler from '../../game_engine/keys_handler';
 
-const GameEngine = ({ gameLoopActivated, currentTetromino, gameVerifierActivated }) => {
+const GameEngine = ({ gameLoopActivated, currentTetromino, gameVerifierActivated, cells }) => {
     const gameVerifier = useRef(new GameVerifier());
     const gameLoop = useRef(new GameLoop());
     const keysHandler = useRef(new KeysHandler());
@@ -20,18 +20,26 @@ const GameEngine = ({ gameLoopActivated, currentTetromino, gameVerifierActivated
             gameLoop.current.stop();
         }
     }, [gameLoopActivated])
+
     useEffect(() => {
         if (gameVerifierActivated) {
-            gameVerifier.current.verify()
+            gameVerifier.current.verifyTetrominoCollideCells()
         }
     }, [currentTetromino, gameVerifierActivated])
+
+    useEffect(() => {
+        gameVerifier.current.verifyLineClear()
+
+    }, [cells])
+
     return (null);
 }
 
 const mapStateToProps = state => ({
     gameLoopActivated: state.gameEngine.gameLoopActivated,
     gameVerifierActivated: state.gameEngine.gameVerifierActivated,
-    currentTetromino: state.currentTetromino
+    currentTetromino: state.currentTetromino,
+    cells: state.cells
 });
 
 export default connect(mapStateToProps)(GameEngine);
